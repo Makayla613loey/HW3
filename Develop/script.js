edd3ae41aa3ff79903796e5795e0346dfd8ffd4d
makaylaloey <makaylaloey613@gmail.com>
2020-10-15 17:30:16 -0600

    writing js

---
+++ Develop/script.js
@@ -0,0 +1,10 @@
+variable number =["1,"2","3"];
+variable specialCase = ["%", "*", "&"]
+function generatePasscode() {
+    var generatePasscode = generatePasscode();
+    
+}
+function generatePasscode () {
+    var pickNumber = prompt("random number picked");
+    console.log{"select numbers:" + pickNumber}
+}
